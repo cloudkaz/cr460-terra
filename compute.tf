d481796da5a9c8a5c4a9c45f6817d4140715264d
cloudkaz <amin.kazoura@polymtl.ca>
2017-03-21 01:08:41 -0400

    added description
    still having issues with firewall rules and tags

--- compute.tf
+++ compute.tf
@@ -1,16 +1,172 @@
-resource "google_compute_instance" "instance1" {
-name          = "instance1"
+// ==============================================
+// Instance: jumphost on debian-8 on public subnetwork
+// ==============================================
+resource "google_compute_instance" "jumphost" {
+name          = "jumphost"
 machine_type  = "f1-micro"
 zone          = "us-east1-b"
+tags          = ["public-mgmt"]
 
 disk {
   image = "debian-cloud/debian-8"
+  auto_delete = true
      }
 
 network_interface {
-  subnetwork = "${google_compute_subnetwork.east1-dmz.name}"
+  subnetwork = "${google_compute_subnetwork.public.name}"
   access_config {}
+    }
 }
 
+// ==============================================
+// Instance: Vault on CoreOS & apache2 on public subnetwork
+// ==============================================
+resource "google_compute_instance" "vault" {
+name          = "vault"
+machine_type  = "f1-micro"
+zone          = "us-east1-b"
+tags          = ["public-web"]
+
+disk {
+  image = "coreos-cloud/coreos-stable"
+  auto_delete = true
+     }
+
+network_interface {
+  subnetwork = "${google_compute_subnetwork.public.name}"
+  access_config {}
+    }
+
 metadata_startup_script = "apt-get -y install apache2 && systemctl start apache2"
+}
+
+// ==============================================
+// Instance: Master debian-8 on backend subnetwork
+// ==============================================
+resource "google_compute_instance" "master" {
+name          = "master"
+machine_type  = "f1-micro"
+zone          = "us-east1-b"
+tags          = ["workload-mgmt"]
+
+disk {
+  image = "coreos-cloud/coreos-stable"
+  auto_delete = true
+     }
+
+network_interface {
+  subnetwork = "${google_compute_subnetwork.backend.name}"
+  access_config {}
+    }
+}
+
+// ==============================================
+// Instance: etcd1 debian-8 on backend subnetwork
+// ==============================================
+resource "google_compute_instance" "etcd1" {
+name          = "etcd1"
+machine_type  = "f1-micro"
+zone          = "us-east1-b"
+tags          = ["backend-mgmt"]
+
+disk {
+  image = "coreos-cloud/coreos-stable"
+  auto_delete = true
+     }
+
+network_interface {
+  subnetwork = "${google_compute_subnetwork.backend.name}"
+  access_config {}
+    }
+}
+
+// ==============================================
+// Instance: etcd2 debian-8 on backend subnetwork
+// ==============================================
+resource "google_compute_instance" "etcd2" {
+name          = "etcd2"
+machine_type  = "f1-micro"
+zone          = "us-east1-b"
+tags          = ["backend-mgmt"]
+
+disk {
+  image = "coreos-cloud/coreos-stable"
+  auto_delete = true
+     }
+
+network_interface {
+  subnetwork = "${google_compute_subnetwork.backend.name}"
+  access_config {}
+    }
+}
+
+// ==============================================
+// Instance: etcd3 debian-8 on backend subnetwork
+// ==============================================
+resource "google_compute_instance" "etcd3" {
+name          = "etcd3"
+machine_type  = "f1-micro"
+zone          = "us-east1-b"
+tags          = ["backend-mgmt"]
+
+disk {
+  image = "coreos-cloud/coreos-stable"
+  auto_delete = true
+     }
+
+network_interface {
+  subnetwork = "${google_compute_subnetwork.backend.name}"
+  access_config {}
+    }
+}
+
+// ==============================================
+// Worker instance template
+// ==============================================
+resource "google_compute_instance_template" "worker-template" {
+  name           = "worker-template"
+  description    = "worker instance template"
+  machine_type   = "f1-micro"
+  can_ip_forward = false
+  tags           = ["workload-mgmt"]
+
+  disk {
+    source_image = "coreos-cloud/coreos-stable"
+    auto_delete  = true
+    boot         = true
+  }
+
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.workload.name}"
+  }
+}
+
+// ==============================================
+// Instance group manager
+// ==============================================
+resource "google_compute_instance_group_manager" "group-manager" {
+  name        = "group-manager"
+  description = "Workker instance group manager"
+  base_instance_name = "worker"
+  instance_template  = "${google_compute_instance_template.worker-template.self_link}"
+  zone               = "us-east1-b"
+}
+
+// ==============================================
+// Instance autoscaler
+// ==============================================
+resource "google_compute_autoscaler" "autoscaler" {
+  name   = "autoscaler"
+  zone   = "us-east1-b"
+  target = "${google_compute_instance_group_manager.group-manager.self_link}"
+
+  autoscaling_policy = {
+    max_replicas    = 5
+    min_replicas    = 2
+    cooldown_period = 60
+
+    cpu_utilization {
+      target = 0.5
+    }
+  }
 }
--- networking.tf
+++ networking.tf
@@ -1,48 +1,131 @@
+// ==============================================
+// Main network for cr460 class
+// ==============================================
 resource "google_compute_network" "cr460" {
   name					          = "cr460"
   auto_create_subnetworks = "false"
 }
 
-resource "google_compute_subnetwork" "east1-dmz" {
-  name				  = "east1-dmz"
+// ==============================================
+// Public web network
+// ==============================================
+resource "google_compute_subnetwork" "public" {
+  name				  = "public"
   ip_cidr_range = "172.16.1.0/24"
   network			  = "${google_compute_network.cr460.self_link}"
   region        = "us-east1"
 }
 
-resource "google_compute_subnetwork" "east1-internal" {
-  name          = "east1-internal"
+// ==============================================
+// Workload network for cr460 class
+// ==============================================
+resource "google_compute_subnetwork" "workload" {
+  name          = "workload"
   ip_cidr_range = "10.0.1.0/24"
   network       = "${google_compute_network.cr460.self_link}"
   region        = "us-east1"
 }
 
-resource "google_compute_firewall" "web" {
-  name = "web"
+// ==============================================
+// Backend network for cr460 class
+// ==============================================
+resource "google_compute_subnetwork" "backend" {
+  name          = "backend"
+  ip_cidr_range = "10.0.2.0/24"
+  network       = "${google_compute_network.cr460.self_link}"
+  region        = "us-east1"
+}
+
+// ==============================================
+// Firewall public network management
+// ==============================================
+resource "google_compute_firewall" "public-mgmt" {
+  name = "public-mgmt"
   network = "${google_compute_network.cr460.name}"
 
   allow {
-    protocol = "tcp"
-    ports = ["80"]
-    }
-    source_ranges = ["0.0.0.0/0"]
-}
-
-resource "google_compute_firewall" "ssh" {
-  name = "ssh"
-  network = "${google_compute_network.cr460.name}"
+    protocol = "icmp"
+  }
 
   allow {
     protocol = "tcp"
     ports = ["22"]
     }
     source_ranges = ["0.0.0.0/0"]
+//    source_tags = ["public-mgmt"]
 }
 
-resource "google_dns_record_set" "www" {
-  name = "www.aminkazoura.cr460lab.com."
+// ==============================================
+// Firewall public network web access
+// ==============================================
+resource "google_compute_firewall" "public-web" {
+  name = "public-web"
+  network = "${google_compute_network.cr460.name}"
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports = ["80", "443"]
+    }
+    source_ranges = ["0.0.0.0/0"]
+//    source_tags = ["public-web"]
+}
+
+// ==============================================
+// Firewall workload network management
+// ==============================================
+resource "google_compute_firewall" "workload-mgmt" {
+  name = "workload-mgmt"
+  network = "${google_compute_network.cr460.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+    }
+    source_ranges = ["${google_compute_subnetwork.public.ip_cidr_range}"]
+//    source_tags = ["workload-mgmt"]
+}
+
+// ==============================================
+// Firewall backend network management
+// ==============================================
+resource "google_compute_firewall" "backend-mgmt" {
+  name = "backend-mgmt"
+  network = "${google_compute_network.cr460.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports = ["22", "2379", "2380"]
+    }
+    source_ranges = ["${google_compute_subnetwork.public.ip_cidr_range}", "${google_compute_subnetwork.workload.ip_cidr_range}"]
+//    source_tags = ["backend-mgmt"]
+}
+
+// ==============================================
+// DNS records for cr460 class (jump & vault)
+// ==============================================
+resource "google_dns_record_set" "jump" {
+  name = "jump.aminkazoura.cr460lab.com."
   type = "A"
   ttl  = "300"
   managed_zone = "aminkazoura-cr460"
-  rrdatas = ["${google_compute_instance.instance1.network_interface.0.access_config.0.assigned_nat_ip}"]
+  rrdatas = ["${google_compute_instance.jumphost.network_interface.0.access_config.0.assigned_nat_ip}"]
+}
+
+resource "google_dns_record_set" "vault" {
+  name = "vault.aminkazoura.cr460lab.com."
+  type = "A"
+  ttl  = "300"
+  managed_zone = "aminkazoura-cr460"
+  rrdatas = ["${google_compute_instance.vault.network_interface.0.access_config.0.assigned_nat_ip}"]
 }
